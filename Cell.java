657e8ce43cf9f07c5d60477b500a67cac2bd4086
Mikecamdo <mcdoherty@smu.edu>
2023-02-28 15:01:42 -0600

    Implemented Cell Class
    Implemented the Cell Class. Includes variables giving row and column position of the cell, as well as variables telling if the cell contains food (for the snake to eat) or if part of the snake is on the cell.

--- Cell.java
+++ Cell.java
@@ -1,3 +1,38 @@
 public class Cell {
-    
+    private int column; //stores column position
+    private int row; //stores row position
+    private boolean hasFood = false; //if cell has food on it
+    private boolean hasSnake = false; //if the snake is on the cell
+
+    //Constructor
+    public Cell(int column, int row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    //Getter and Setter methods
+    public void setHasFood(boolean hasFood) {
+        this.hasFood = hasFood;
+    }
+
+    public boolean getHasFood() {
+        return hasFood;
+    }
+
+    public void setHasSnake(boolean hasSnake) {
+        this.hasSnake = hasSnake;
+    }
+
+    public boolean getHasSnake() {
+        return hasSnake;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
 }
