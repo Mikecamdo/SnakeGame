3e4adcd97a34708e4dd34ee8ec416aa66bfc4462
Mikecamdo <mcdoherty@smu.edu>
2023-02-28 16:25:09 -0600

    Creation of a GUI
    Updated Board so it is compatible with JFrame. Added a Launcher class that starts the game. Began implementing Game (which will contain the Board and Snake objects and will create the GUI).

--- Board.java
+++ Board.java
@@ -1,13 +1,18 @@
 import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
 
-public class Board {
+public class Board extends JPanel {
     private Cell[][] theBoard; //the playing surface
+    private int numRows = 20;
+    private int numColumns = 20;
+    private int cellSize = 20;
 
     //Constructor
     public Board() {
-        theBoard = new Cell[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        theBoard = new Cell[numRows][numColumns];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
                 theBoard[i][j] = new Cell(i, j);
             }
         }
@@ -26,11 +31,28 @@ public class Board {
     public void addFood() { //randomly selects a cell to add food to
         Random generator = new Random();
         while (true) {
-            int row = generator.nextInt(10);
-            int column = generator.nextInt(10);
+            int row = generator.nextInt(numRows);
+            int column = generator.nextInt(numColumns);
             if (theBoard[row][column].getHasSnake() == false) {
                 theBoard[row][column].setHasFood(true);
                 return;
+            }
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(numColumns * cellSize, numRows * cellSize);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
+                int x = j * cellSize;
+                int y = i * cellSize;
+                g.drawRect(x, y, cellSize, cellSize);
             }
         }
     }
--- Game.java
+++ Game.java
@@ -1,3 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Game {
-    
+    private Board theBoard = new Board();
+
+    class BoardPanel extends JPanel {
+        private static final int ROWS = 20;
+        private static final int COLUMNS = 20;
+        private int sqSize = 20;
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(COLUMNS * sqSize, ROWS * sqSize);
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLUMNS; j++) {
+                    int x = j * sqSize;
+                    int y = i * sqSize;
+                    g.drawRect(x, y, sqSize, sqSize);
+
+                }
+            }
+
+        }
+    }
+
+    public void run() {
+        JFrame theWindow = new JFrame();
+        theWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theWindow.add(theBoard);
+        theWindow.pack();
+        theWindow.setLocationRelativeTo(null);
+        theWindow.setVisible(true);
+    }
 }
