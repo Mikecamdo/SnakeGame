70672febbd3932cb77b8bfe0ce19d3c20275d4e5
Mikecamdo <mcdoherty@smu.edu>
2023-02-28 15:22:25 -0600

    Implemented Board Class
    Implemented the Board class (which consists of a matrix of Cell objects). Also added an addFood() method that randomly generates food on the board.

--- Board.java
+++ Board.java
@@ -1,3 +1,38 @@
+import java.util.Random;
+
 public class Board {
-    
+    private Cell[][] theBoard; //the playing surface
+
+    //Constructor
+    public Board() {
+        theBoard = new Cell[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                theBoard[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    //Getter and Setter methods
+    public void setTheBoard(Cell[][] theBoard) {
+        this.theBoard = theBoard;
+    }
+
+    public Cell[][] getTheBoard() {
+        return theBoard;
+    }
+
+    //Methods
+    public void addFood() { //randomly selects a cell to add food to
+        Random generator = new Random();
+        while (true) {
+            int row = generator.nextInt(10);
+            int column = generator.nextInt(10);
+            if (theBoard[row][column].getHasSnake() == false) {
+                theBoard[row][column].setHasFood(true);
+                return;
+            }
+        }
+    }
+
 }
