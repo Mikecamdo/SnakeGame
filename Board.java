a8dadaf775c5cb9615607eb8b947e900c2b42753
Mikecamdo <mcdoherty@smu.edu>
2023-03-16 19:20:41 -0500

    Deleted Game class
    Moved all relevant code from Game.java to Board.java

--- Board.java
+++ Board.java
@@ -3,8 +3,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.Image;
 import java.sql.RowIdLifetime;
-
 import javax.swing.ImageIcon;
+import java.util.LinkedList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 
 public class Board extends JPanel {
     private Cell[][] theBoard; //the playing surface
@@ -14,6 +19,9 @@ public class Board extends JPanel {
     private int cellSize = 20;
     private int foodX;
     private int foodY;
+    private String[] directions = {"left", "right", "up", "down"};
+    private String currentDirection = "right";
+    private boolean gameOver = false;
 
     private Image head;
     private Image body;
@@ -48,7 +56,41 @@ public class Board extends JPanel {
         return theSnake;
     }
 
+    public void setDirections(String[] directions) {
+        this.directions = directions;
+    }
+
+    public String[] getDirections() {
+        return directions;
+    }
+
+    public void setCurrentDirection(String currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public String getCurrentDirection() {
+        return currentDirection;
+    }
+
     //Methods
+    public void run() {
+        JFrame theWindow = new JFrame();
+        // theBoard.loadImages();
+
+        theWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theWindow.add(new Board());
+        theWindow.pack();
+        theWindow.setResizable(false);
+        theWindow.setLocationRelativeTo(null);
+        theWindow.setVisible(true);
+        theWindow.addKeyListener(new Input());
+        //int p = 98;
+        
+        // while (gameOver == false) {
+        //     theBoard.m
+        // }
+    }
+
     public void addFood() { //randomly selects a cell to add food to
         Random generator = new Random();
         while (true) {
@@ -61,6 +103,28 @@ public class Board extends JPanel {
                 return;
             }
         }
+    }
+
+    public Cell getNext(Cell current, String direction) {
+        int row = current.getRow();
+        int column = current.getColumn();
+
+        if (direction == "left") {
+            column -= 1;
+        } else if (direction == "down") {
+            row += 1;
+        } else if (direction == "right") {
+            column += 1;
+        } else if (direction == "up") {
+            row -= 1;
+        }
+
+        Cell next = theBoard[row][column];
+        return next;
+    }
+
+    public void move() {
+        theSnake.move(getNext(theSnake.getHead(), currentDirection));
     }
 
     // public void loadImages() {
@@ -93,16 +157,18 @@ public class Board extends JPanel {
 
         if(true){
             g.setColor(Color.red);
-            g.fillOval(foodX, foodY,cellSize,cellSize);
-            for(int i=0;i<2;i++){
-                if(i==0){
-                    g.setColor(Color.green);
-                    g.fillRect(20,20,cellSize,cellSize);
-                }
-                else{
-                    g.setColor(new Color(50,180,0));
-                    g.fillRect(40,40,cellSize,cellSize);
-                }
+            g.fillOval(foodX, foodY, cellSize, cellSize);
+
+            Cell head = theSnake.getHead();
+            LinkedList<Cell> body = theSnake.getBody();
+            int length = theSnake.getBody().size();
+
+            g.setColor(Color.green);
+            g.fillRect(head.getRow() * cellSize, head.getColumn() * cellSize, cellSize, cellSize);
+            
+            for(int i = 0; i < length; i++) {
+                g.setColor(new Color(50,180,0));
+                g.fillRect(body.get(i).getRow() * cellSize, body.get(i).getColumn() * cellSize, cellSize, cellSize);
             }
         //    g.setColor(Color.blue);
         //    g.setFont(new Font("Ink Free",Font.BOLD,40));
@@ -111,5 +177,27 @@ public class Board extends JPanel {
 
         }
     }
+
+    public class Input extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+
+            if ((keyCode == KeyEvent.VK_LEFT) && (currentDirection != "right")) {
+                currentDirection = "left";
+                System.out.println("Left");
+            } else if ((keyCode == KeyEvent.VK_DOWN) && (currentDirection != "up")) {
+                currentDirection = "down";
+                System.out.println("Down");
+            } else if ((keyCode == KeyEvent.VK_RIGHT) && (currentDirection != "left")) {
+                currentDirection = "right";
+                System.out.println("Right");
+            } else if ((keyCode == KeyEvent.VK_UP) && (currentDirection != "down")) {
+                currentDirection = "up";
+                System.out.println("Up");
+            }
+        }
+    }
+
 
 }
